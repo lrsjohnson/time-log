b609c6e61e3fa78742caf6ca0f51f7aae9155955
Lars Johnson <larsj@mit.edu>
2013-10-05 15:02:56 -0400

    Added TimeEntry model.

--- tracker/timelog/models.py
+++ tracker/timelog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class TimeEntry(models.Model):
+    title = models.CharField(max_length=100)
+    start_time = models.DateTimeField('start')
+    end_time = models.DateTimeField('end')
+
+    def __unicode__(self):
+        return self.title
